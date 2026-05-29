b7b7fe3c1fde0b387715bdcd47fdf1ca7f30446d
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-29 20:47:56 +0530

    my c practice

---
+++ week 1/age_cal.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main(){
+  int  num1, num2, ans;
+printf("Enter your birth year:");
+scanf("%d", &num1);
+printf("Enter current year:");
+scanf("%d", &num2);
+ans = num2 - num1;
+printf("your age is: %d",ans);
+getchar();
+return 0;
+}
