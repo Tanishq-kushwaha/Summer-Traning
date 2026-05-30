006ed0ddc966218a852927e181da6dd90ec14aeb
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-30 21:37:09 +0530

    average_of_five_sub

---
+++ week 1/average_of_five_sub.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int first, second, third, fourth, fifth, sum, average;
+  printf("Enter your first subject marks :");
+  scanf("%d",&first);
+  printf("Enter your second subject marks :");
+  scanf("%d",&second);
+  printf("Enter your third subject marks :");
+  scanf("%d",&third);
+  printf("Enter your fourth subject marks :");
+  scanf("%d",&fourth);
+  printf("Enter your fifth subject marks :");
+  scanf("%d",&fifth);
+  sum = first + second + third + fourth + fifth;
+  average = (float)sum/5;
+  printf("Average is : %d", average);
+  return 0;
+}
