68cb89016e7a38cffc9ab42ee5d9ca5d62e3dacb
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-30 21:11:21 +0530

    feat: convert total days into years, weeks, and remaining days

--- week 1/days_to_time.c
+++ week 1/days_to_time.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main(){
-  int x, y, days, weeks, year;
+  int remainingDays, remainingDaysAfterWeeks, days, weeks, year;
   printf("Enter Total days : ");
   scanf("%d",&days);
   year = days/365;
   printf("Year : %d \n",year);
-  x = days%365;
-  weeks = x/7;
+  remainingDays = days%365;
+  weeks = remainingDays/7;
   printf("Weeks: %d\n",weeks);
-   y = x % 7;
-  printf("Day : %d\n",y);
+   remainingDaysAfterWeeks = remainingDays % 7;
+  printf("Day : %d\n",remainingDaysAfterWeeks);
   return 0;
 }
