3c9ccad502de68a6044dea1e2047a36fa69df150
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-05-30 19:50:49 +0530

    some changes in .exe file

--- week 1/celsius_to_fahrenheit_calculator.c
+++ week 1/celsius_to_fahrenheit_calculator.c
@@ -1,10 +1,10 @@
 // celsius_to_fahrenheit_calculator
 #include<stdio.h>
-int main(){
-float C,F;
-printf("Enter the Temperature in  Celsius :");
-scanf("%f",&C);
-F = C*(9.0/5.0)+32;
-printf("Temperature in Fahrenheit : %f",F);
-return 0;
+   int main(){
+   float C,F;
+   printf("Enter the Temperature in  Celsius :");
+   scanf("%f",&C);
+   F = C*(9.0/5.0)+32;
+   printf("Temperature in Fahrenheit : %f",F);
+   return 0;
 }        
