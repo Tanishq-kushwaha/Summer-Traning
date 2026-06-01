972ef740b1a19b168e2dd0c81ef93b01361327cf
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-01 19:59:32 +0530

    The Even/Odd Check

--- week 2/evenOdd.c
+++ week 2/evenOdd.c
@@ -1,11 +1,11 @@
 // Problem 1: The Even/Odd Check
 #include<stdio.h>
 int main(){
-    int a;
+    int num;
     printf("The Even/Odd Check \n");
     printf("Enter the number :");
-    scanf("%d",&a);
-    if(a % 2 == 0){
+    scanf("%d",&num);
+    if(num % 2 == 0){
         printf("Number is Even");
     }else{
         printf("Number is Odd");
---
+++ week 2/tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+The Even/Odd Check
