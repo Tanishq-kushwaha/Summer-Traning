2324a31d1047bcee271066a320770d0f6366e8a3
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-01 20:07:14 +0530

    /*  * Program: Age Gate Eligibility System  * Objective: Input the user's current age and verify voting eligibility  *            using relational operators .  */

--- week 2/ageGate.c
+++ week 2/ageGate.c
@@ -1,4 +1,9 @@
 // Problem 2: The Age Gate
+/*
+ * Program: Age Gate Eligibility System
+ * Objective: Input the user's current age and verify voting eligibility 
+ *            using relational operators.
+ */
 #include<stdio.h>
 int main(){
 
