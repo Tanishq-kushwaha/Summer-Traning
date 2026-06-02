9ae353cd8fee2eed701cfd0b184df5fcea154bfd
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-02 17:50:03 +0530

    feat :  Area of a Rectangle

---
+++ week 1/home work/areaOfARectangle.c
@@ -0,0 +1,15 @@
+/*
+ * Program: Area of a Rectangle
+ * Objective: Accept length and width from the user to calculate the total area of a rectangle.
+ */
+
+ #include<stdio.h>
+ int main(){
+    int length, width, area;
+    printf("Enter the length of rectangle : ");
+    scanf("%d",&length);
+    printf("Enter the width of rectangle : ");
+    scanf("%d",&width);
+    area = length * width;
+    printf("The Area of rectangle : %d",area);
+ }
