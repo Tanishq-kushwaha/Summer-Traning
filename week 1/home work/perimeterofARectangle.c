20190f87c47cb4b963d092c466b6aa314d916cea
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-02 17:58:48 +0530

    feat : Perimeter of a Rectangle

--- week 1/home work/perimeterofARectangle.c
+++ week 1/home work/perimeterofARectangle.c
@@ -3,4 +3,14 @@
  * Objective: Accept length and width from the user to calculate the total perimeter boundary.
  */
 
- 
+ #include<stdio.h>
+ int main(){
+    int length, width, perimeter;
+    printf("Enter the length of rectangle : ");
+    scanf("%d",&length);
+    printf("Enter the width of rectangle : ");
+    scanf("%d",&width);
+    perimeter = 2*(length + width);
+    printf("The Perimeter is  : %d",perimeter);
+    return 0;
+ }
