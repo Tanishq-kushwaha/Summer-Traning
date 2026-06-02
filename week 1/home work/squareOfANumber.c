46229844083fae2d5f0c0078b5aeb2ac026a5559
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-02 17:20:32 +0530

    feat : Square of a Number

--- week 1/home work/squareOfANumber.c
+++ week 1/home work/squareOfANumber.c
@@ -2,3 +2,12 @@
  * Program: Square of a Number
  * Objective: Input a single number from the user and calculate its mathematical square value.
  */
+ #include<stdio.h>
+ int main(){
+    int num, square;
+    printf("Enter the number :");
+    scanf("%d",&num);
+    square = num * num;
+    printf("Your Answer is: %d",square);
+    return 0;
+ } 
