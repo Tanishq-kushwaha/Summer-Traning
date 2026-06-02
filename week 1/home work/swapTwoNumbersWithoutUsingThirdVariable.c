fc879f22c9c659298168fb9f51bacf4a04506beb
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-02 16:55:30 +0530

    feat : Swap Two Numbers (Without Third Variable)

---
+++ week 1/home work/swapTwoNumbersWithoutUsingThirdVariable.c
@@ -0,0 +1,8 @@
+/*
+ * Program: Swap Two Numbers (Without Third Variable)
+ * Objective: Swap the values of two variables using arithmetic operations without any auxiliary variable.
+ */
+ #include<stdio.h>
+ int main(){
+    
+ }
