f18f1436fa1fd6c9ce51b854f1833ef0cfdcc6d0
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-02 15:48:19 +0530

    feet : Program: Remainder Finder

---
+++ week 1/home work/remainderFinder.c
@@ -0,0 +1,16 @@
+/*
+ * Program: Remainder Finder
+ * Objective: Accept two integers from the user and calculate the remainder using the modulo (%) operator.
+ */
+
+#include<stdio.h>
+int main(){
+    int num1, num2, remainder;
+    printf("Enter the Dividend : \n");
+    scanf("%d",&num1);
+    printf("Enter the Divisor : \n");
+    scanf("%d",&num2);
+    remainder = num1 % num2;
+    printf("Remainder is : %d",remainder);
+    return 0;
+}
