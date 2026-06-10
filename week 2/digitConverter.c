fb1c924c5e72f6571ad2fd077669f34e47973850
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-10 20:00:46 +0530

    feat: add digit counter using while loop and integer division

--- week 2/digitConverter.c
+++ week 2/digitConverter.c
@@ -5,15 +5,18 @@
  * incrementing a counter variable until the number reduces to zero.
  */
  #include<stdio.h>
- #include<conio.h>
  int main(){
     int num;
     printf("Enter your number : ");
     scanf("%d",&num);
     int count = 0;
+    if(num == 0){
+        count = 1;
+    }else{
     while(num > 0){
        num = num/10;
        count++;
     }
+}
     printf("The total digit count is : %d", count);
  }
