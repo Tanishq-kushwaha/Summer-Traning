01da63bdaf27375b4ef602c1813ba62f33321c1a
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-10 20:37:02 +0530

    feat: implement main function to compute sum of natural numbers

--- week 2/sumOfNaturalNumbers.c
+++ week 2/sumOfNaturalNumbers.c
@@ -1,5 +1,17 @@
 /*
  * Program: Sum of Natural Numbers
  * Objective: Compute the cumulative sum of all natural numbers from 1 up to a user-defined integer N.
- * Logic: Implement a for loop iterating from 1 to N, continuously updating a running total variable.
  */
+ #include<stdio.h>
+ int main(){
+    int num;
+    int i = 1;
+    int sum = 0;
+    printf("Enter your number : ");
+    scanf("%d",&num);
+
+    for(i; i <= num ; i++){
+        sum = sum + i;
+    }
+    printf("Sum is : %d", sum);
+ }
