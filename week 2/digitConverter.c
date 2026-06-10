2f69352b33822b236fa565c4e82ddd2f478995e6
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-10 20:15:17 +0530

    refactor: simplify digit counting logic and remove redundant conditions

--- week 2/digitConverter.c
+++ week 2/digitConverter.c
@@ -1,8 +1,6 @@
 /*
  * Program: Digit Counter
  * Objective: Accept an integer input from the user and determine the total count of its digits.
- * Logic: Use a while loop to repeatedly perform integer division by 10, 
- * incrementing a counter variable until the number reduces to zero.
  */
  #include<stdio.h>
  int main(){
@@ -13,18 +11,13 @@
     if(num == 0){
         count = 1;
     }
-    else if(num < 0){
-      num = num*-1;
-      while(num > 0){
-       num = num/10;
-       count++;
+    if(num < 0){
+      num = -num;
     }
-    }
-    else{
     while(num > 0){
        num = num/10;
        count++;
     }
-}
+
     printf("The total digit count is : %d", count);
  }
