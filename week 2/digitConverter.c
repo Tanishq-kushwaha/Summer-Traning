bab13a7767dffc85a2c97dc896b719584bc0f6cd
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-10 19:41:24 +0530

    feat: add digit counter program to count the number of digits in an integer

---
+++ week 2/digitConverter.c
@@ -0,0 +1,6 @@
+/*
+ * Program: Digit Counter
+ * Objective: Accept an integer input from the user and determine the total count of its digits.
+ * Logic: Use a while loop to repeatedly perform integer division by 10, 
+ * incrementing a counter variable until the number reduces to zero.
+ */
