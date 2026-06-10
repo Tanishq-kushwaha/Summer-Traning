20662a3c31f36f6bdc0fff4eff3bcf5c74587866
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-10 20:29:35 +0530

    feat: add initial implementation for summing natural numbers

---
+++ week 2/sumOfNaturalNumbers.c
@@ -0,0 +1,5 @@
+/*
+ * Program: Sum of Natural Numbers
+ * Objective: Compute the cumulative sum of all natural numbers from 1 up to a user-defined integer N.
+ * Logic: Implement a for loop iterating from 1 to N, continuously updating a running total variable.
+ */
