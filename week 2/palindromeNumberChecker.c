258d823c983b3e8aa4c386013903442fca8de94c
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-11 13:31:24 +0530

    feat: enhance palindrome checker with number reversal logic and negative number handling

--- week 2/palindromeNumberChecker.c
+++ week 2/palindromeNumberChecker.c
@@ -1,13 +1,31 @@
 /*
  * Program: Palindrome Number Checker
  * Objective: Verify if a user-input integer reads the same backward as forward.
-
  */
  #include<stdio.h>
  int main(){
     int num , originalNum;
+    int lastDigit;
+    int isNegative = 0;
+    int reverse = 0;
     printf("Enter your number : ");
     scanf("%d",&num);
 
+     originalNum = num;
+     if(num < 0){
+       isNegative = 1;
+       num = -num;
+     }
+    while(num > 0){
+       lastDigit  = num % 10;
+       reverse = reverse * 10 + lastDigit;
+       num = num /10; 
+    }
+    if(isNegative){
+     reverse = -reverse;
+    }
 
+     printf("The reversed number is: %d\n", reverse);
+    return 0;
+    
  }
