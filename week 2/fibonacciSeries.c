51cb5a4ef182f3ffa189935c55079c67d79b7ce5
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-13 13:38:27 +0530

    fix: correct syntax errors in Fibonacci Series Generator program

--- week 2/fibonacciSeries.c
+++ week 2/fibonacciSeries.c
@@ -4,4 +4,13 @@
  * Logic: Maintain two tracking variables for the first two terms (0 and 1), and use a loop 
  * to continuously calculate and print the next term by summing the previous two.
  */
- #include<
+ #include<stdio.h>
+ int main(){
+    int num;
+    printf(Enter your number of terms : );
+    scanf("%d"&num);
+
+    for(int i = 0; i < num ; i++){
+    
+    }
+ }
