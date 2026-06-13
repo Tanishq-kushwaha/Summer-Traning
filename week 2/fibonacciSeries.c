997ddb6e18213ffad8d0d1e8458384203c5b4f30
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-13 13:22:23 +0530

    feat: add Fibonacci Series Generator program with initial documentation

---
+++ week 2/fibonacciSeries.c
@@ -0,0 +1,7 @@
+/*
+ * Program: Fibonacci Series Generator
+ * Objective: Display the Fibonacci sequence up to a user-specified number of terms (N).
+ * Logic: Maintain two tracking variables for the first two terms (0 and 1), and use a loop 
+ * to continuously calculate and print the next term by summing the previous two.
+ */
+ #include<
