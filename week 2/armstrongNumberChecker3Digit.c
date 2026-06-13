569996e35f33e6e6fe38d252ec8bfa0c8c6c8e09
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-13 12:40:49 +0530

    feat: implement basic structure for Armstrong number checker program

--- week 2/armstrongNumberChecker3Digit.c
+++ week 2/armstrongNumberChecker3Digit.c
@@ -4,3 +4,14 @@
  * Logic: Extract each digit using the modulo operator (%), calculate its cube, accumulate 
  * the sum of these cubes, and perform an equality check (==) against the original input.
  */
+
+ #include<stdio.h>
+ int main(){
+    int num;
+    printf("Enter your three digit number : ");
+    scanf("%d"%num);
+
+    while(num > 0){
+        
+    }
+ }
