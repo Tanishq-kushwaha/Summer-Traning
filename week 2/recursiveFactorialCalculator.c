74ca562757c0cd7e911868c4887cc589934b6114
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-14 22:20:37 +0530

    feat: implement recursive factorial calculator with user input

--- week 2/recursiveFactorialCalculator.c
+++ week 2/recursiveFactorialCalculator.c
@@ -3,3 +3,24 @@
  * Objective: Calculate the factorial of a given integer using recursive function calls.
  * Logic: Use a base case to return 1 when n is 0 or 1. Otherwise, recursively return n * factorial(n - 1).
  */
+ #include<stdio.h>
+
+ int calculateFactorial(int n);
+
+ int main(){
+    int n;
+    int fact;
+   printf("Enter your number : ");
+   scanf("%d",&n);
+   fact = calculateFactorial(n);
+   printf("Factorial is %d", fact);
+   return 0;
+ }
+
+int calculateFactorial(int n){
+ if(n == 1 || n == 0 ){
+    return 1;
+ }else{
+    return n* calculateFactorial(n - 1);
+ }
+ }
