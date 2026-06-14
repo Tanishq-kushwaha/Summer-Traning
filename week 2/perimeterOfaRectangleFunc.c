1e969d35ed8b6de31cf1c45f5dbb4ea5a642aafe
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-14 19:44:53 +0530

    feat: implement function to calculate the perimeter of a rectangle

---
+++ week 2/perimeterOfaRectangleFunc.c
@@ -0,0 +1,16 @@
+// Challenge 3: The Complete Execution (Write the Code)
+// Write a complete C program to calculate the Perimeter of a Rectangle.
+// Constraint 1: You must write a proper Function Declaration above main().
+// Constraint 2: You must write the Function Definition below main().
+// Constraint 3: main() should handle taking the user inputs (length and width) and passing them to the function.
+
+#include<stdio.h>
+int perimeter(int a, int b)
+{
+    int p;
+    p = 2(a + b);
+    return p;
+}
+int main(){
+perimeter(10, 5);
+}
