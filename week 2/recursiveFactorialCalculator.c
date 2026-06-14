341ff70b445fb651968f5f9a894e41a57625c8c4
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-14 22:14:13 +0530

    feat: add initial implementation of recursive factorial calculator

---
+++ week 2/recursiveFactorialCalculator.c
@@ -0,0 +1,5 @@
+/*
+ * Program: Recursive Factorial Calculator
+ * Objective: Calculate the factorial of a given integer using recursive function calls.
+ * Logic: Use a base case to return 1 when n is 0 or 1. Otherwise, recursively return n * factorial(n - 1).
+ */
