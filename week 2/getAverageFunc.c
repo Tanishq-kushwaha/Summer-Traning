18f3ce930ee341b7aee4a5d6ea576705557f3016
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-14 19:20:16 +0530

    fix: add parentheses to return statement in getAverage function

--- week 2/getAverageFunc.c
+++ week 2/getAverageFunc.c
@@ -4,8 +4,17 @@ int getAverage(int a, int b, int c)
     int sum, average;
     sum = a + b + c;
     average = sum / 3;
-    return average;
+    return (average);
 }
 int main(){
-
+    int a, b,  c;
+printf("Enter your no : ");
+scanf("%d", &a);
+printf("Enter your no : ");
+scanf("%d", &b);
+printf("Enter your no : ");
+scanf("%d", &c);
+// int z = getAverage(a, b, c); 
+printf("%d", getAverage(a, b, c));
+return 0;
 }
