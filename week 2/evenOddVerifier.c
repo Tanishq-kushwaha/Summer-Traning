ceb9dc98385df4d2cb9ba30dab67d3467ef48671
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-14 21:39:50 +0530

    feat: modularize even/odd check with dedicated function

--- week 2/evenOddVerifier.c
+++ week 2/evenOddVerifier.c
@@ -4,3 +4,29 @@
  * Logic: The function takes an integer, uses modulo (%), and returns 1 if Even, or 0 if Odd.
  * main() will use this returned integer to print the final message.
  */
+
+ #include<stdio.h>
+
+int checkEvenOdd(int num);
+
+int main(){
+  int num;
+  int result;
+  printf("Enter your no :");
+  scanf("%d",&num);
+    result = checkEvenOdd(num);
+ if(result == 1){
+printf("Even");
+}else{
+    printf("Odd");
+ }
+ return 0;
+}
+
+int checkEvenOdd(int num){
+  if(num%2 == 0){
+    return 1;
+  }else{
+    return 0;
+  }
+}
