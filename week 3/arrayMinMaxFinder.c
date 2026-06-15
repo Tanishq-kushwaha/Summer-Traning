1ae4e2bcbeb50bc6f2d6e82bd7d576640ccc5db9
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-15 16:47:18 +0530

    feat: initialize max and min variables in Array Min-Max Finder program

--- week 3/arrayMinMaxFinder.c
+++ week 3/arrayMinMaxFinder.c
@@ -7,9 +7,23 @@
  int main(){
     int arr[5];
     int i;
+    int max = arr[0];
+    int min = arr[0];
     for(i = 0; i < 5;i++){
         printf("Enter your value : ");
         scanf("%d",&arr[i]);
  }
+ for(i = 1; i < 5; i++){
+      if(arr[i] > max){
+        max = arr[i];
+       }
+     if(arr[i] < min){
+      min = arr[i];
+       }
+ }
+
+printf("min value is : %d \n", min);
+printf("Max value is : %d \n", max);
+
  return 0;
 }
