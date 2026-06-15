01d2962344c2c493b8c49156a556fd44d1f80de5
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-15 16:57:51 +0530

    feat: add complete implementation of Array Min-Max Finder program

--- week 3/tempCodeRunnerFile.c
+++ week 3/tempCodeRunnerFile.c
@@ -1,3 +1,29 @@
-   // for(int i = 0; i < 5; i++){
-    //     sum = sum + arr[i];
-    // }
+/*
+ * Program: Array Min-Max Finder
+ * Objective: Read 5 integers into an array and determine the largest and smallest elements.
+ */
+
+ #include<stdio.h>
+ int main(){
+    int arr[5];
+    int i;
+    for(i = 0; i < 5;i++){
+        printf("Enter your value : ");
+        scanf("%d",&arr[i]);
+    }
+    int max = arr[0];
+    int min = arr[0];
+    for(i = 1; i < 5; i++){
+       if(arr[i] > max){
+        max = arr[i];
+          }
+       if(arr[i] < min){
+         min = arr[i];
+        }
+    }
+
+printf("min value is : %d \n", min);
+printf("Max value is : %d \n", max);
+
+ return 0;
+}
