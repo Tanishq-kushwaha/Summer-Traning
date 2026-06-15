f0e3be2f44af5a58f9c3968fcd786ad30679e0ce
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-15 16:19:20 +0530

    feat: add initial implementation of Array Min-Max Finder program

---
+++ week 3/arrayMinMaxFinder.c
@@ -0,0 +1,10 @@
+/*
+ * Program: Array Min-Max Finder
+ * Objective: Read 5 integers into an array and determine the largest and smallest elements.
+ */
+
+ #include<stdio.h>
+ int main(){
+    int arr[5];
+    
+ }
