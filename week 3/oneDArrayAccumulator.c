f88684c3963e5c0cd25509d496622a1b996fdff6
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-15 16:09:12 +0530

    feat: implement 1D Array Accumulator to compute sum of user inputs

--- week 3/oneDArrayAccumulator.c
+++ week 3/oneDArrayAccumulator.c
@@ -2,3 +2,16 @@
  * Program: 1D Array Accumulator
  * Objective: Read 5 integer inputs from the user into an array and compute their total sum.
  */
+
+ #include<stdio.h>
+ int main(){
+    int arr[5];
+    int sum = 0;
+    for( int i = 0; i < 5; i++){
+        printf("Enter your number : ");
+        scanf("%d",&arr[i]);
+         sum = sum + arr[i];
+    }
+    printf("Sum is : %d", sum);
+    return 0;
+ }
