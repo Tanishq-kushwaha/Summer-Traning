21c2a5ac4ec5c14f33ac3d238cece11d76debc96
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-15 17:20:58 +0530

    feat: implement initial version of In-Place Array Reversal program

--- week 3/inPlaceArrayReversal.c
+++ week 3/inPlaceArrayReversal.c
@@ -2,3 +2,16 @@
  * Program: In-Place Array Reversal
  * Objective: Reverse the elements of a 1D array directly in memory using the two-pointer technique.
  */
+ #include<stdio.h>
+ int main(){
+  int n;
+  printf("Enter your Array size : ");
+  scanf("%d",&n);
+
+  int arr[n];
+  for(int i = 0; i < n; i++){
+    printf("Enter your value : ");
+    scanf("%d",&arr[i]);
+  }
+  return 0;
+ }
