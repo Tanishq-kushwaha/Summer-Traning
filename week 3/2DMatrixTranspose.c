4814c839dbf9191b50f1dbf36d6d33da04a74646
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-17 16:53:40 +0530

    fix: correct loop structure for printing transposed matrix

--- week 3/2DMatrixTranspose.c
+++ week 3/2DMatrixTranspose.c
@@ -19,8 +19,12 @@
     matrix[1][0] = temp;    
 
     //  Transpose Matrix
+ printf("Transpose Matrix \n");
   for(int i = 0; i<2;i++){
-    for(int j = 0;)
+    for(int j = 0; j<2; j++){
+        printf("%d ",matrix[i][j]);
+    }
+    printf("\n");
   }
     return 0;
  }
