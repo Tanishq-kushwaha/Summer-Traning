3dc1c10e3f2f8db1cdc6f6b76881e0d2f92f2a73
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-17 16:51:21 +0530

    feat: implement input and swap logic for 2D matrix transpose

--- week 3/2DMatrixTranspose.c
+++ week 3/2DMatrixTranspose.c
@@ -4,5 +4,23 @@
  */
  #include<stdio.h>
  int main(){
-    
+    int matrix[2][2];
+    int temp;
+    printf("Enter your matrix \n");
+    for(int i = 0; i<2; i++){
+        for(int j = 0; j<2; j++){
+            printf("Enter your value [%d][%d] :", i, j);
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    //swap
+    temp = matrix[0][1];
+    matrix[0][1] = matrix[1][0];
+    matrix[1][0] = temp;    
+
+    //  Transpose Matrix
+  for(int i = 0; i<2;i++){
+    for(int j = 0;)
+  }
+    return 0;
  }
