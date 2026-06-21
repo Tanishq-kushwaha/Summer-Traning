be89e8a9b9c66d90256e5b85ef5a88cb1f681708
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-21 18:34:08 +0530

    feat: implement basic structure for Prime Number Checker program

--- week 3/PrimeNumberChecker.c
+++ week 3/PrimeNumberChecker.c
@@ -2,3 +2,12 @@
  * Program: Optimized Prime Number Checker
  * Objective: Determine if a user-input integer is a prime number.
  */
+
+ #include<stdio.h>
+ int main(){
+    int n;
+    int isPrime = 1;
+    printf("Enter your number : ");
+    scanf("%d",%n);
+    
+ }
