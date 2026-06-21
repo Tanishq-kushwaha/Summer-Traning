2f39908baf6deaad4a2b34d5a371332524bae624
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-21 18:49:56 +0530

    feat: implement prime number checking logic in PrimeNumberChecker

--- week 3/PrimeNumberChecker.c
+++ week 3/PrimeNumberChecker.c
@@ -10,4 +10,21 @@
     printf("Enter your number : ");
     scanf("%d",%n);
     
+
+    if(n <= 1){
+        isPrime = 0;
+    }else{
+        for(int i = 2; i <= n/2;i++){
+            if(n%i == 0){
+               isPrime = 0;
+               break;  
+            }
+        }
+    }
+    if(isPrime == 1){
+    printf("Prime Number");
+    }else{
+      printf("Not a Prime Number");   
+    }
+    return 0;
  }
