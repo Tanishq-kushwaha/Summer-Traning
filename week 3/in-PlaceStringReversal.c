a354657dc618352d8970e44e88a39fe480494aa1
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-24 21:07:30 +0530

    feat: implement In-Place String Reversal functionality with user input and length calculation

--- week 3/in-PlaceStringReversal.c
+++ week 3/in-PlaceStringReversal.c
@@ -3,3 +3,16 @@
  * Objective: Reverse a user-input character array directly in memory without using <string.h>.
  * Note: Designed using the Two-Pointer technique to ensure O(1) space complexity.
  */
+#include<stdio.h>
+int main(){
+    char str[50];
+    printf("Enter your string : ");
+    scanf("%s", str);
+      
+    int strLength = 0;
+    for(int i = 0; str[i] != '\0'; i++){
+     strLength++;
+    }
+    printf("%d",&strLength);
+    return 0;
+}
