caaf7e6b9d8d39e0123c6c09fb3330d61798e11f
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-24 20:09:51 +0530

    feat: add initial documentation for In-Place String Reversal program

---
+++ week 3/in-PlaceStringReversal.c
@@ -0,0 +1,5 @@
+/*
+ * Program: In-Place String Reversal
+ * Objective: Reverse a user-input character array directly in memory without using <string.h>.
+ * Note: Designed using the Two-Pointer technique to ensure O(1) space complexity.
+ */
