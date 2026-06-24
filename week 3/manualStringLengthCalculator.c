198c0d7dc74ad5e57991f0c2fc9a500393e14e38
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-24 19:46:00 +0530

    feat: add initial implementation of Manual String Length Calculator

---
+++ week 3/manualStringLengthCalculator.c
@@ -0,0 +1,9 @@
+/*
+ * Program: Manual String Length Calculator
+ * Objective: Determine the character length of a user-input string without using built-in library functions.
+ */
+
+ #include<stdio.h>
+ int main(){
+
+ }
