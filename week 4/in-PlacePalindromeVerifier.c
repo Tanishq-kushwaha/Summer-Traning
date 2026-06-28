11bbb0b306b3795183aad9788eff9be95f93fe6e
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-28 13:53:24 +0530

    feat: implement main function and user input for In-Place Palindrome Verifier

--- week 4/in-PlacePalindromeVerifier.c
+++ week 4/in-PlacePalindromeVerifier.c
@@ -3,3 +3,11 @@
  * Objective: Determine if a character array is a palindrome without using extra memory or <string.h>.
  * Note: Leverages manual length calculation and a two-pointer comparison strategy.
  */
+ #include<stdio.h>
+ int main(){
+    char str[10];
+
+    printf("Enter your string");
+    scanf("%s", str);
+    return 0;
+ }
