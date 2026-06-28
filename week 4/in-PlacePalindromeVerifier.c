9199495aebab931477696f2d9d9ea0ee6bfb9e00
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-28 12:59:10 +0530

    feat: add header comments for In-Place Palindrome Verifier program

---
+++ week 4/in-PlacePalindromeVerifier.c
@@ -0,0 +1,5 @@
+/*
+ * Program: In-Place Palindrome Verifier
+ * Objective: Determine if a character array is a palindrome without using extra memory or <string.h>.
+ * Note: Leverages manual length calculation and a two-pointer comparison strategy.
+ */
